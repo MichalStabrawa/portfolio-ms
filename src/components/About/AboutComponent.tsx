5f2970d40cc403363e5310986aac4b814855870c
Michal Stabrawa <mistaaqua@gmail.com>
2024-01-10 11:56:35 +0100

    add About

---
+++ src/components/About/AboutComponent.tsx
@@ -0,0 +1,11 @@
+import classes from "./AboutComponent.module.scss";
+
+function AboutComponent() {
+  return (
+    <section className={classes.about}>
+      <h2>About Me</h2>
+    </section>
+  );
+}
+
+export default AboutComponent;
